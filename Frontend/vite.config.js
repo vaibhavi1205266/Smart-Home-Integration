fb9e96e3941cb200c513ad40d45c294a6ba2ea92
Nizam <102827156+Nizam6239@users.noreply.github.com>
2025-04-10 00:15:12 +0530

    Update vite.config.js
    chnnn

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests to the backend server
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: 'https://67f6b8ac86e73e5327b6ccd9--mernfoodapp1.netlify.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '') // Optional: Adjust the path if needed
